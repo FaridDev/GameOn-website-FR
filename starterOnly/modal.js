4389d5842db2b2ab2fa7488e524f5ae8c606ae62
Farid Amimri <farid.amimri@gmail.com>
2021-06-22 11:27:22 +0200

    close modal event for the popup

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelectorAll(".close");
+const firstInput = document.querySelector('input');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +21,36 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal event
+close.forEach((span) => span.addEventListener("click", closeModal));
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// ckeck input validity
+firstInput.addEventListener('input', () => {
+  firstInput.setCustomValidity(' ');
+  firstInput.checkValidity();
+});
+
+// invalid event with error message
+firstInput.addEventListener('invalid', () => {
+  if(firstInput.value === '') {
+    firstInput.setCustomValidity('Veuillez saisir votre prénom !');
+  } else {
+    firstInput.setCustomValidity('Votre prénom ne doit contenir que des lettres');
+  }
+});
+
+
+
+
+
+
+
+
 
 
