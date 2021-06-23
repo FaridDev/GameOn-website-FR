3c75f1cd2a31f362c9867f5b5c780c4e47802dbe
Farid Amimri <farid.amimri@gmail.com>
2021-06-23 16:24:28 +0200

    add submit function for lastName & email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,8 +12,11 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const close = document.querySelectorAll(".close");
-const firstName = document.querySelector('#first');
 const form = document.querySelector('form');
+const firstName = document.querySelector('#first');
+const lastName = document.querySelector('#last');
+const email = document.querySelector('#email');
+
 
 
 // launch modal event
@@ -32,12 +35,15 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// form submit
+form.addEventListener("submit", formSubmit);
+
 // form submit function for firstName
-function firstNameValidator () {
+function firstNameValidator() {
   const divParent = firstName.parentNode
-  if (firstName.value.length<2) {
+  if (firstName.value.length < 2) {
     divParent.setAttribute('data-error-visible', 'true')
-    divParent.setAttribute('data-error', 'Message Erreur')
+    divParent.setAttribute('data-error', 'Veuillez taper votre prénom')
     return false
   } else {
     return true
@@ -49,9 +55,40 @@ function formSubmit(e) {
   firstNameValidator()
 };
 
-// form submit
-form.addEventListener("submit", formSubmit);
 
+// form submit function for lastName
+function lastNameValidator() {
+  const divParent = lastName.parentNode
+  if (lastName.value.length < 2) {
+    divParent.setAttribute('data-error-visible', 'true')
+    divParent.setAttribute('data-error', 'Veuillez taper votre nom')
+    return false
+  } else {
+    return true
+  }
+};
+
+function formSubmit(e) {
+  e.preventDefault()
+  lastNameValidator()
+};
+
+// form submit function for email
+function emailValidator() {
+  const divParent = email.parentNode
+  if (email.value.length = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g')) {
+    divParent.setAttribute('data-error-visible', 'true')
+    divParent.setAttribute('data-error', 'Veuillez taper votre email')
+    return false
+  } else {
+    return true
+  }
+};
+
+function formSubmit(e) {
+  e.preventDefault()
+  emailValidator()
+};
 
 
 
