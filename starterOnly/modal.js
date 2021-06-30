6c7649acb25ee118feab9eeb84a6354ea2f6ffe4
Farid Amimri <farid.amimri@gmail.com>
2021-06-30 11:16:51 +0200

    function form for birthday updated

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -37,71 +37,108 @@ function closeModal() {
 // form submit
 form.addEventListener("submit", formSubmit);
 
+function formSubmit(e) {
+  const submit = document.querySelectorAll('.btn-submit');
+  e.preventDefault()
+  // const isValid = firstNameValidator() && lastNameValidator() && emailValidator() && checkDate() && checkNumber() && checkRadio() && checkTerms()
+  const isValid = checkDate()
+  if (isValid) {
+    modalbg.style.display = "block";
+  };
+};
+
 // form submit function for firstName
 function firstNameValidator() {
   const divParent = firstName.parentNode
   if (firstName.value.length < 2) {
     divParent.setAttribute('data-error-visible', 'true')
-    divParent.setAttribute('data-error', 'Veuillez taper votre prénom')
+    divParent.setAttribute('data-error', 'Vous devez entrer votre prénom')
     return false
   } else {
-    return true
+    divParent.setAttribute('data-error-visible', 'false')
+    return true 
   }
 };
-
-function formSubmit(e) {
-  e.preventDefault()
-  firstNameValidator()
-};
-
 
 // form submit function for lastName
 function lastNameValidator() {
   const divParent = lastName.parentNode
   if (lastName.value.length < 2) {
     divParent.setAttribute('data-error-visible', 'true')
-    divParent.setAttribute('data-error', 'Veuillez taper votre nom')
+    divParent.setAttribute('data-error', 'Vous devez entrer votre nom')
     return false
   } else {
+    divParent.setAttribute('data-error-visible', 'false')
     return true
   }
-};
-
-function formSubmit(e) {
-  e.preventDefault()
-  lastNameValidator()
 };
 
 // form submit function for email
 function emailValidator() {
   const divParent = email.parentNode
-  if (email.value.length = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g')) {
+  const emailReg = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g')
+  if (!emailReg.test(email.value)) {
     divParent.setAttribute('data-error-visible', 'true')
-    divParent.setAttribute('data-error', 'Veuillez taper votre email')
+    divParent.setAttribute('data-error', 'Vous devez entrer votre email')
     return false
   } else {
+    divParent.setAttribute('data-error-visible', 'false')
     return true
   }
 };
 
-function formSubmit(e) {
-  e.preventDefault()
-  emailValidator()
+// form submit function for birthDate
+function checkDate() {
+  const divParent = birthDay.parentNode
+  const birthDay = document.querySelector('#birthdate').value;
+  const today = new Date(); // Today date
+  const birthDate = new Date(birthDay).value;
+  if (birthDate > today) {
+    divParent.setAttribute('data-error-visible', 'true')
+    divParent.setAttribute('data-error', 'Vous devez entrer une date de naissance valide')
+    return false
+  } else {
+    divParent.setAttribute('data-error-visible', 'false')
+    return true
+  }
 };
 
-// form submit function for birthDate
-const birthDate = document.querySelector('#birthdate');
-const today = new Date().toISOString().split('T')[0];
+// Form submit function checking number
+function checkNumber() {
+  const quantity = document.querySelector('#quantity').value;
+  const quantityReg = new RegExp('^[0-9]+$'); // valeur entre 0 et 9 à entrer
+  if (!quantityReg.test(quantity)) {
+    divParent.setAttribute('data-error-visible', 'true')
+    divParent.setAttribute('data-error', 'Vous devez entrer un chiffre')
+    return false;
+  }
+  return true;
+};
 
-const validDate = () => {
-  let day = new Date(today);
-  day.setDate(day.getDate() - 1);
-  let birthday = day.toISOString().split('T')[0];
-  birthDate.value = birthday;
-  birthDate.max = birthday;
-}
-validDate();
-console.log(birthDate);
+// Form submit function radio button validation
+function checkRadio() {
+  const locationList = document.getElementsByName('location');
+  let hasChecked = false;
+  for(let i=0; i < locationList.length; i++) {
+    if (locationList[i].checked == true) {
+      hasChecked = true;
+    }
+  }
+  return hasChecked;
+};
+
+// Form submit function checking terms and conditions if valid
+function checkTerms () {
+  const agreeBox = document.querySelector('#checkbox1'); 
+  console.log(agreeBox.checked)
+  if (!agreeBox.checked) { //if the box is checked
+      alert("Veuillez accepter les termes et conditions !");
+      return false;
+  }
+  return true;
+};
+
+
 
 
 
