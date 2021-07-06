ad487276f03643a3d0bb2f5a56770af9b1bcacc7
Farid Amimri <farid.amimri@gmail.com>
2021-07-06 08:46:42 +0200

    faunction for validation form updated

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -16,6 +17,11 @@ const close = document.querySelectorAll(".close");
 const firstName = document.querySelector('#first');
 const lastName = document.querySelector('#last');
 const email = document.querySelector('#email');
+const birthDay = document.getElementById('birthdate');
+const quantity = document.querySelector('#quantity');
+const locationList = document.getElementsByName('location');
+const agreeBox = document.querySelector('#checkbox1');
+
 
 
 // launch modal event
@@ -34,36 +40,35 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-// form submit
+// ******* form submit *******
 form.addEventListener("submit", formSubmit);
 
 function formSubmit(e) {
   const submit = document.querySelectorAll('.btn-submit');
   e.preventDefault()
-  // const isValid = firstNameValidator() && lastNameValidator() && emailValidator() && checkDate() && checkNumber() && checkRadio() && checkTerms()
-  const isValid = checkDate()
+  const isValid = firstNameValidator() && lastNameValidator() && emailValidator() && checkDate() && checkNumber() && checkRadio() && checkTerms()
   if (isValid) {
     modalbg.style.display = "block";
   };
 };
 
-// form submit function for firstName
+// ******* form submit function for firstName ******* //
 function firstNameValidator() {
   const divParent = firstName.parentNode
-  if (firstName.value.length < 2) {
+  if (firstName.value.length < 2) { // Minimum de 2 caractères
     divParent.setAttribute('data-error-visible', 'true')
     divParent.setAttribute('data-error', 'Vous devez entrer votre prénom')
     return false
   } else {
     divParent.setAttribute('data-error-visible', 'false')
-    return true 
+    return true
   }
 };
 
-// form submit function for lastName
+// ******* form submit function for lastName ******* //
 function lastNameValidator() {
   const divParent = lastName.parentNode
-  if (lastName.value.length < 2) {
+  if (lastName.value.length < 2) { // Minimum de 2 caractères
     divParent.setAttribute('data-error-visible', 'true')
     divParent.setAttribute('data-error', 'Vous devez entrer votre nom')
     return false
@@ -73,11 +78,11 @@ function lastNameValidator() {
   }
 };
 
-// form submit function for email
+// ******* form submit function for email ******* //
 function emailValidator() {
   const divParent = email.parentNode
-  const emailReg = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g')
-  if (!emailReg.test(email.value)) {
+  const emailReg = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g') // Expression régulière à respecter
+  if (!emailReg.test(email.value)) { // On teste l'email sur la base de la RegExp
     divParent.setAttribute('data-error-visible', 'true')
     divParent.setAttribute('data-error', 'Vous devez entrer votre email')
     return false
@@ -87,13 +92,21 @@ function emailValidator() {
   }
 };
 
-// form submit function for birthDate
+// ******* form submit function for birthDate ******* //
+const today = new Date().toISOString().split('T')[0]; // Date d'aujourd'hui au format jj/mm/aaaa
+
+const birthDate = () => {
+  let day = new Date(today); // On part de la date du jour
+  day.setDate(day.getDate() - 1); // Initialisation de la date à J-1
+  let birth = day.toISOString().split('T')[0]; // Conversion au format jj/mm/aaaa
+  birthDay.value = birth;
+};
+birthDate();
+
 function checkDate() {
   const divParent = birthDay.parentNode
-  const birthDay = document.querySelector('#birthdate').value;
-  const today = new Date(); // Today date
-  const birthDate = new Date(birthDay).value;
-  if (birthDate > today) {
+
+  if (birthDate < today) {
     divParent.setAttribute('data-error-visible', 'true')
     divParent.setAttribute('data-error', 'Vous devez entrer une date de naissance valide')
     return false
@@ -103,39 +116,48 @@ function checkDate() {
   }
 };
 
-// Form submit function checking number
+// ******* Form submit function checking number ******* //
 function checkNumber() {
-  const quantity = document.querySelector('#quantity').value;
+  const divParent = quantity.parentNode
   const quantityReg = new RegExp('^[0-9]+$'); // valeur entre 0 et 9 à entrer
-  if (!quantityReg.test(quantity)) {
+  if (!quantityReg.test(quantity)) { // On teste la valeur sur la base de la RegExp
     divParent.setAttribute('data-error-visible', 'true')
     divParent.setAttribute('data-error', 'Vous devez entrer un chiffre')
     return false;
+  } else {
+    divParent.setAttribute('data-error-visible', 'false')
+    return true
   }
-  return true;
 };
 
-// Form submit function radio button validation
+// ******* Form submit function radio button validation ******* //
 function checkRadio() {
-  const locationList = document.getElementsByName('location');
+  const divParent = locationList.parentNode
   let hasChecked = false;
-  for(let i=0; i < locationList.length; i++) {
-    if (locationList[i].checked == true) {
+  for (let i = 0; i < locationList.length; i++) {
+    if (locationList[i].checked == true) { // si une des cases est cochée
       hasChecked = true;
+      divParent.setAttribute('data-error-visible', 'false')
+    } else {
+      hasChecked = false;
+      divParent.setAttribute('data-error-visible', 'true')
+      divParent.setAttribute('data-error', 'Veuillez choisir une ville')
     }
   }
   return hasChecked;
 };
 
-// Form submit function checking terms and conditions if valid
-function checkTerms () {
-  const agreeBox = document.querySelector('#checkbox1'); 
-  console.log(agreeBox.checked)
-  if (!agreeBox.checked) { //if the box is checked
-      alert("Veuillez accepter les termes et conditions !");
-      return false;
+// ******* Form submit function for terms and conditions validation *******
+function checkTerms() {
+  const divParent = agreeBox.parentNode
+  if (!agreeBox.checked) { // si la case n'est pas cochée
+    divParent.setAttribute('data-error-visible', 'true')
+    divParent.setAttribute('data-error', 'Vous devez vérifier que vous acceptez les termes et conditions.')
+    return false;
+  } else {
+    divParent.setAttribute('data-error-visible', 'false')
+    return true;
   }
-  return true;
 };
 
 
